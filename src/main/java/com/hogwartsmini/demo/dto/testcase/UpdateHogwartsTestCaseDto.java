74a23e176a01d16fd4c7d1a71d1d8317b0f92bee
tlibn <1039085737@qq.com>
2022-02-26 18:17:32 +0800

    定向6-20220226直播代码8

---
+++ src/main/java/com/hogwartsmini/demo/dto/testcase/UpdateHogwartsTestCaseDto.java
@@ -0,0 +1,35 @@
+package com.hogwartsmini.demo.dto.testcase;
+
+import com.hogwartsmini.demo.entity.BaseEntityNew;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@ApiModel(value="修改测试用例对象")
+@Data
+public class UpdateHogwartsTestCaseDto extends BaseEntityNew {
+    /**
+     * 主键
+     */
+    @ApiModelProperty(value="测试用例主键",required=true)
+    private Integer id;
+
+    /**
+     * 测试用例数据
+     */
+    @ApiModelProperty(value="测试用例数据",required=true)
+    private String caseData;
+
+    /**
+     * 用例名称
+     */
+    @ApiModelProperty(value="测试用例名称",required=true)
+    private String caseName;
+
+    /**
+     * 备注
+     */
+    @ApiModelProperty(value="测试用例备注",required=true)
+    private String remark;
+
+}
---
+++ src/main/java/com/hogwartsmini/demo/util/StrUtil.java
@@ -0,0 +1,40 @@
+package com.hogwartsmini.demo.util;
+
+import com.hogwartsmini.demo.common.Constants;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *@Author tlibn
+ *@Date 2022/2/21 14:57
+ **/
+public class StrUtil {
+
+    public static Integer getUserId(HttpServletRequest request) {
+        String userIdStr = request.getHeader(Constants.TOKEN);
+        Integer userId = null;
+        try{
+            userId = Integer.parseInt(userIdStr);
+        }catch (Exception e){
+            throw new RuntimeException("用户id必须为数字");
+        }
+        return userId;
+    }
+
+    public static String list2Ids(List<Integer> caseIdList) {
+        return caseIdList.toString().replace("[","").replace("]","");
+    }
+
+    public static void main(String[] args) {
+
+        List<Integer> caseIdList = new ArrayList<>();
+        caseIdList.add(1);
+        caseIdList.add(2);
+        caseIdList.add(3);
+
+        System.out.println(list2Ids(caseIdList));
+    }
+
+}
