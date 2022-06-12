9bd6b38f90c4c93864c396683927cb9b7cb42330
tlibn <1039085737@qq.com>
2022-06-12 17:47:46 +0800

    常量类

---
+++ src/main/java/com/hogwartsmini/demo/common/Constants.java
@@ -0,0 +1,38 @@
+package com.hogwartsmini.demo.common;
+
+/**
+ * 通用相关常量
+ */
+public interface Constants {
+
+
+	/**
+	 * 未删除
+	 */
+	public final static Integer DEL_FLAG_ONE = 1;
+
+	/**
+	 * 已删除
+	 */
+	public final static Integer DEL_FLAG_ZERO = 0;
+
+	/**
+	 * 0 无效
+	 */
+	public final static Integer STATUS_ZERO = 0;
+
+	/**
+	 * 1 新建
+	 */
+	public final static Integer STATUS_ONE = 1;
+	/**
+	 * 任务完成 3
+	 */
+	public final static Integer STATUS_THREE = 3;
+
+	/**
+	 * 用户id
+	 */
+	public final static String TOKEN = "token";
+
+}
