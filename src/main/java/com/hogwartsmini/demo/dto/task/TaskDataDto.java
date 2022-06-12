0f18001cbdc6b6d067970df0c6e0a3d886b8a67e
tlibn <1039085737@qq.com>
2022-06-12 17:45:56 +0800

    任务dto

---
+++ src/main/java/com/hogwartsmini/demo/dto/task/TaskDataDto.java
@@ -0,0 +1,32 @@
+package com.hogwartsmini.demo.dto.task;
+
+import com.hogwartsmini.demo.common.BaseDto;
+import com.sun.istack.internal.NotNull;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@ApiModel(value="执行测试任务类",description="请求参数类" )
+@Data
+public class TaskDataDto extends BaseDto {
+
+    /**
+     * 任务数量
+     */
+    @ApiModelProperty(value="任务数量",required=true)
+    @NotNull
+    private Integer taskCount;
+
+    /**
+     * 分类的key
+     */
+    @ApiModelProperty(value="分类的key", required = true)
+    private Integer taskKey;
+
+    /**
+     * 描述
+     */
+    @ApiModelProperty(value="描述")
+    private String desc;
+
+}
